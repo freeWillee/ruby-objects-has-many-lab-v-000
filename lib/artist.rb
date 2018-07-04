e767820ec23e38b8c6087b2b4c5f988a82d2624b
Will Lee <will.i.lee@outlook.com>
2018-07-04 17:11:43 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,29 +2,29 @@ require 'pry'
 
 class Artist
   attr_accessor :songs, :name
-
+  @@songs
   def initialize(name)
     @name = name
-    @songs = []
+    @@songs = []
   end
 
   def songs
-    @songs
+    @@songs
   end
 
   def add_song(song)
-    @songs << song
+    @@songs << song
     song.artist = self
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
-    @songs << song
+    @@songs << song
     song.artist = self
   end
 
   def self.song_count
-    @songs.count
+    @@songs.count
   end
 
 end
