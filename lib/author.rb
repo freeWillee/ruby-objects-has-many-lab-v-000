f6c8339fdf9541e310816d8b48ca683d0d5276a1
Will Lee <will.i.lee@outlook.com>
2018-07-04 17:48:03 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,28 +1,29 @@
 class Author
   attr_accessor :name
 
+  @@posts = []
+
   def initialize(name)
     @name = name
-    @posts = []
   end
 
   def add_post(post)
-    @posts << post
+    @@posts << post
     post.author = self
   end
 
   def posts
-    @posts
+    @@posts
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
-    @posts << post
+    @@posts << post
     post.author = self
   end
 
   def self.post_count
-    @posts.count
+    @@posts.count
   end
 
 end
